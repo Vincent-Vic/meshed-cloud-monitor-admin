40e043f252dd24c80859f95b0cc7e3586c564a23
Vincent Vic <Vic.Coding@gmail.com>
2023-04-28 21:45:22 +0800

    新增配置

---
+++ src/main/java/cn/meshed/cloud/config/Constants.java
@@ -0,0 +1,17 @@
+package cn.meshed.cloud.config;
+
+/**
+ * <h1></h1>
+ *
+ * @author Vincent Vic
+ * @version 1.0
+ */
+public interface Constants {
+
+    /** 默认日期时间格式 */
+    String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    /** 默认日期格式 */
+    String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    /** 默认时间格式 */
+    String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+}
